2990925ef0a10debe4ba017e7c29fa9d498690d0
Md. Ashad khan <asadkhan409684@gmail.com>
2026-04-06 00:49:40 +0600

    add categories file

---
+++ config/categories.php
@@ -0,0 +1,26 @@
+<?php
+// Centralized Menu Categories
+$category_labels = [
+    'breakfast' => 'Breakfast',
+    'platter' => 'Platters',
+    'meal-deal' => 'Meal Deals',
+    'signature' => 'Signature Dishes',
+    'pizza' => 'Pizza',
+    'burger' => 'Burger',
+    'pasta & chowmein' => 'Pasta & Chowmein',
+    'sandwiches' => 'Sandwiches',
+    'savory waffle' => 'Savory Waffle',
+    'soup & ramen' => 'Soup & Ramen',
+    'fresh salad' => 'Fresh Salad',
+    'dessert' => 'Dessert',
+    'sugary waffle' => 'Sugary Waffle',
+    'frappuccino' => 'Frappuccino',
+    'mocktail' => 'Mocktail',
+    'milk shake' => 'Milk Shake',
+    'fresh juice' => 'Fresh Juice',
+    'hot coffee' => 'Hot Coffee',
+    'iced coffee' => 'Iced Coffee',
+    'side dishes' => 'Side Dishes',
+    'early meal add ons' => 'Early Meal Add-ons'
+];
+?>
